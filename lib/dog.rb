450078ef326775b97bd91c5b008e200a1e3589c6
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-23 08:41:14 +0300

    add file

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    def name=(dogs_name)
+        #local var; local scope.
+        @this_dogs_name = dogs_name
+        #@-instance var tag.
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
